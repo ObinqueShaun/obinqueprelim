aef4628d32dae2cfbf598b283752133d25866619
ObinqueShaun <obinqueshaunyeshua@gmail.com>
2025-08-19 23:25:36 -0700

    newprelim

---
+++ src/setaobinque/SetAobinque.java
@@ -0,0 +1,17 @@
+package setaobinque;
+
+import java.util.Scanner;
+
+public class SetAobinque {
+
+
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        
+        double hourly_wage;
+        int hours;
+        
+    }
+    
+}
